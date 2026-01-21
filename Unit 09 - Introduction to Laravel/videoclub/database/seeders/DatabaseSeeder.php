fd471cf4f8174dc3fc84d5a4b743b6c712f7c341
Gabriel Daniel Manea <gabimanea222@gmail.com>
2026-01-21 14:26:23 +0100

    Starting seeder part

--- Unit 09 - Introduction to Laravel/videoclub/database/seeders/DatabaseSeeder.php
+++ Unit 09 - Introduction to Laravel/videoclub/database/seeders/DatabaseSeeder.php
@@ -2,24 +2,36 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Catalog;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
+    /**
+     * Seed the movie catalog
+     */
+    private function seedCatalog()
+    {
+        $peliculas = require __DIR__ . '/arrayPeliculas.php';
+
+        foreach ($peliculas as $pelicula) {
+            Catalog::create([
+                'title'    => $pelicula['title'],
+                'year'     => $pelicula['year'],
+                'director' => $pelicula['director'],
+                'poster'   => $pelicula['poster'],
+                'rented'   => $pelicula['rented'],
+                'synopsis' => $pelicula['synopsis']
+            ]);
+        }
+    }
 
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->seedCatalog();
+        $this->command->info('Tabla catálogo inicializada con datos!');
     }
 }
